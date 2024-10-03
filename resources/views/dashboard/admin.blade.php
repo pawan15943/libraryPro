adeecb7c718c03b6059d4ce1e42ae1d71cedc018
Pawan Rathore <samvad.pawan@gmail.com>
2024-10-03 08:18:27 +0530

    Deisgn

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0/dist/chartjs-plugin-datalabels.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
 <div class="row">
     <div class="col-lg-6">
         <div class="row g-4">
@@ -35,9 +38,9 @@
         </div>
     </div>
     <div class="col-lg-3">
-        <div class="dashibox">
+        <div class="dashibox h-my ">
             <h4>Available Seats</h4>
-            <ul>
+            <ul class="contents">
                 @foreach($available_seats as $key => $value)
                 <li>
                     <div class="d-flex">
@@ -49,13 +52,11 @@
                     </div>
                 </li>
                 @endforeach
-             
-               
             </ul>
         </div>
     </div>
     <div class="col-lg-3">
-        <div class="dashibox">
+        <div class="dashibox h-my">
             <h4>Expired in 5 Days</h4>
             <span>Show Expired in 5 Days Seats & Extended Seats </span>
             <ul>
@@ -70,11 +71,21 @@
                     </div>
                 </li>
                 @endforeach
-                
+
             </ul>
         </div>
     </div>
 
 </div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
+<script>
+    (function($) {
+        $(window).on("load", function() {
+            $(".contents").mCustomScrollbar();
+        });
+    })(jQuery);
+</script>
 @endsection

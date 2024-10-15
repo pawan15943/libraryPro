ace9e89b0f2f3a3419d37203cb7d26113b9fd186
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2024-10-15 12:42:37 +0530

    login condition continue2

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
     <div class="d-flex">
         <div class="conatent flex">
             <i class="fa fa-bars mr-2" id="sidebar"></i>
-            @if(isset($librarydiffInDays) && Auth::guard('library')->check() && !$is_renew)
+            @if(isset($librarydiffInDays) && Auth::guard('library')->check() && !$is_renew && $isProfile)
                 
             <small class="text-danger ml-2"> <i class="fa fa-clock"></i> Plan Expired in {{$librarydiffInDays}} Days</small>
             @if($librarydiffInDays <= 5 && $librarydiffInDays >= 0 && $isProfile)

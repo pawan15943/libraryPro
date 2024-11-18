8c286fcc8a2f1ad1f9e8ff5843e62504f9d962ec
Pawan Rathore <samvad.pawan@gmail.com>
2024-11-18 20:23:08 +0530

    My Design Changes

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -158,12 +158,36 @@
     <!-- End -->
 
     <!-- Library Revenue -->
-    <h4 class="my-4">Monthly Revenues</h4>
     <div class="row g-4">
-        <div class="col-lg-9">
+        <div class="col-lg-8">
+            <h4 class="my-4">Monthly Revenues</h4>
+
             <div class="v-content">
                 <ul class="revenue-box scroll-x " id="monthlyData"></ul>
             </div>
+        </div>
+        <div class="col-lg-4">
+        <h4 class="my-4">Recent Activity</h4>
+        <ul class="activity contents">
+            <li>New User Added on Seat 10
+                <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+            <li>Seat 3 Swaped with Seat 12
+            <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+            <li>Seat No 10 is deleted.
+            <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+            <li>Seat 14 Booked for First Half
+            <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+            <li>Seat 14 Plan Upgrade Houlry Slot 1 to Full Day
+            <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+            <li>Seat 10 Enter in Extnded Mode Today
+            <span class="mt-1"><i class="fa fa-clock"></i> 18-11-2024 10:20:45 AM</span>
+            </li>
+        </ul>
         </div>
     </div>
     <!-- End -->
@@ -883,7 +907,7 @@
             childList: true,
             subtree: true
         });
-        
+
     })(jQuery);
 </script>
 <script>

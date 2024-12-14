dd532824af186081362a409d97607f74aa229c13
Pawan Rathore <samvad.pawan@gmail.com>
2024-12-14 14:47:23 +0530

    Admin Design Changes

--- resources/views/notification/show.blade.php
+++ resources/views/notification/show.blade.php
@@ -7,12 +7,14 @@
 
 <div class="row justify-content-center">
     <div class="col-lg-8">
+        <h4 class="mb-4 text-center">All Notifications</h4>
         <ul class="notification-list">
             @foreach($notifications as $index => $notification)
             @php
             $data = json_decode($notification->data, true);
             @endphp
-            <li>
+            <li class="unread-bg">
+                <div class="unread"></div>
                 <h4>{{ $data['title'] ?? 'N/A' }} <span class="text-success">(Active)</span></h4>
                 <p>{{ $data['description'] ?? 'N/A' }}</p>
                 <div class="d-flex">
@@ -30,57 +32,7 @@
             @endforeach
         </ul>
 
-        <div class="table-responsive">
-            <table class="table text-center datatable border-bottom">
-                <thead>
-                    <tr>
-                        <th>S.No.</th>
-
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Image</th>
-                        <th>URL</th>
-                        <th>Start Date</th>
-                        <th>Expiry Date</th>
-                        <th>Status</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($notifications as $index => $notification)
-                    @php
-                    $data = json_decode($notification->data, true);
-                    @endphp
-                    <tr>
-                        <td>{{ $index + 1 }}</td>
-
-                        <td>{{ $data['title'] ?? 'N/A' }}</td>
-                        <td>{{ $data['description'] ?? 'N/A' }}</td>
-                        <td>
-                            @if(!empty($data['image']))
-                            <img src="{{ $data['image'] }}" alt="Notification Image" style="width: 50px; height: auto;">
-                            @else
-                            N/A
-                            @endif
-                        </td>
-                        <td>
-                            @if(!empty($data['link']))
-                            <a href="{{ $data['link'] }}" target="_blank">View</a>
-                            @else
-                            N/A
-                            @endif
-                        </td>
-                        <td>{{ $notification->start_date }}</td>
-                        <td>{{ $notification->end_date }}</td>
-                        <td>
-                            {{ now()->between($notification->start_date, $notification->end_date) ? 'Active' : 'Expired' }}
-                        </td>
-
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+        
     </div>
 </div>
 

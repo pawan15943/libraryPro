f2f36cd9b5d5ad1bb7b9468c53354b750e123615
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-03-20 09:12:20 +0530

    debug13

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -341,10 +341,7 @@ class LibraryController extends Controller
             $month = ($request->plan_mode == 2) ? 12 : 1;
             $subscription_id=$request->subscription_id;
             $amount=$request->price;
-            dd([
-                'subscription_id'=>$subscription_id,
-                'amount'=>$amount,
-            ]);
+           
         }else{
            
             return redirect('subscriptions.choosePlan')->with('error', 'Plan not selected');

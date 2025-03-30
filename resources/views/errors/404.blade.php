0ee2d6d8a25ea9c5ae8fd42e0ec1a61d498bc7b1
Pawan Rathore <samvad.pawan@gmail.com>
2025-03-30 07:19:34 +0530

    Changes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,9 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <img src="{{ asset('public/img/404.png') }}" alt="404">
-
-            <a href="{{route('/')}}" class="btn btn-primary button">Go to Dashbaord >></a>
+            <div class="error-box">
+                <img src="" alt="">
+                <img src="{{ asset('public/img/404.png') }}" alt="404" class="img-fluid">
+                <h4 class="text-center"><a href="{{route('/')}}" class="btn btn-primary button">Go to Dashbaord >></a></h4>
+            </div>
         </div>
     </div>
 </div>

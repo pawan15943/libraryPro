f66bf25d34f06f1dab04f73f1815eb7bd23569be
Pawan Rathore <samvad.pawan@gmail.com>
2025-04-08 22:30:38 +0530

    add jquery defer to website

--- resources/views/sitelayouts/layout.blade.php
+++ resources/views/sitelayouts/layout.blade.php
@@ -83,11 +83,11 @@
     @yield('content')
     @include('sitelayouts.footer')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.21.0/jquery.validate.min.js" ></script>
-    <script src="{{ url('public/js/main-validation.js') }}" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.21.0/jquery.validate.min.js" defer></script>
+    <script src="{{ url('public/js/main-validation.js') }}" defer></script>
 
     <script>
         $(document).on('click', '.showmore', function() {
